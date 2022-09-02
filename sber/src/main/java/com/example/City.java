4e75571a3cab6f38f92d75f8a63dbc40ca44d7be
johnsmith <iea2001@mail.ru>
2022-09-02 23:39:21 +0700

    Create new class for describe the city

---
+++ sber/src/main/java/com/example/City.java
@@ -0,0 +1,50 @@
+package com.example;
+
+
+public class City {
+    private final String name;
+    private final String region;
+    private final String district;
+    private final int population;
+    private final String foundation;
+
+    public City(String name, String region, String district, int population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation=" + foundation +
+                '}' + '\n';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+}
